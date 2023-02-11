8877f1bc3e454eb21336c6f2d820b14f2ae738f4
Snownee <i@snownee.com>
2023-02-11 18:58:07 +0800

    fix: unexpected class-loading

---
+++ src/main/java/snownee/jade/addon/JadeAddonsBase.java
@@ -0,0 +1,51 @@
+package snownee.jade.addon;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
+import net.fabricmc.loader.api.FabricLoader;
+import snownee.jade.addon.create.CreatePlugin;
+import snownee.jade.addon.lootr.LootrPlugin;
+import snownee.jade.addon.mi.MIPlugin;
+import snownee.jade.addon.secret_rooms.SecretRoomsPlugin;
+import snownee.jade.addon.team_reborn_energy.TeamRebornEnergyPlugin;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaCommonRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
+
+@WailaPlugin
+public class JadeAddonsBase implements IWailaPlugin {
+	public static final Map<String, Supplier<Supplier<IWailaPlugin>>> PLUGIN_LOADERS = Maps.newHashMap();
+	private final List<IWailaPlugin> plugins = Lists.newArrayList();
+
+	static {
+		PLUGIN_LOADERS.put("create", () -> CreatePlugin::new);
+		PLUGIN_LOADERS.put("lootr", () -> LootrPlugin::new);
+		PLUGIN_LOADERS.put("mi", () -> MIPlugin::new);
+		PLUGIN_LOADERS.put("secretrooms", () -> SecretRoomsPlugin::new);
+		PLUGIN_LOADERS.put("team_reborn_energy", () -> TeamRebornEnergyPlugin::new);
+	}
+
+	public JadeAddonsBase() {
+		PLUGIN_LOADERS.forEach((modid, loader) -> {
+			if (FabricLoader.getInstance().isModLoaded(modid)) {
+				plugins.add(loader.get().get());
+			}
+		});
+	}
+
+	@Override
+	public void register(IWailaCommonRegistration registration) {
+		plugins.forEach($ -> $.register(registration));
+	}
+
+	@Override
+	public void registerClient(IWailaClientRegistration registration) {
+		plugins.forEach($ -> $.registerClient(registration));
+	}
+}
