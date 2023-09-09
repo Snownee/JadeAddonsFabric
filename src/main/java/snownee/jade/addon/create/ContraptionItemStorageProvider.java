2f24d2784769ff817c745bee16ca6c127d6171af
Snownee <i@snownee.com>
2023-09-09 15:10:37 +0800

    feat: contraption container caching

--- src/main/java/snownee/jade/addon/create/ContraptionItemStorageProvider.java
+++ src/main/java/snownee/jade/addon/create/ContraptionItemStorageProvider.java
@@ -1,14 +1,19 @@
 package snownee.jade.addon.create;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
 import com.simibubi.create.content.contraptions.Contraption;
 
+import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
+import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
+import snownee.jade.addon.universal.ItemCollector;
+import snownee.jade.addon.universal.ItemStorageProvider;
 import snownee.jade.api.Accessor;
 import snownee.jade.api.view.ClientViewGroup;
 import snownee.jade.api.view.IClientExtensionProvider;
@@ -33,8 +38,14 @@ public enum ContraptionItemStorageProvider implements IServerExtensionProvider<A
 
 	@Override
 	public List<ViewGroup<ItemStack>> getGroups(ServerPlayer player, ServerLevel level, AbstractContraptionEntity entity, boolean showDetails) {
+		//TODO: simplify it in 1.20.2+
 		Contraption contraption = ((AbstractContraptionEntity) entity).getContraption();
-		return List.of(JadeFabricUtils.fromItemStorage(contraption.getSharedInventory(), 54, 0));
+		Storage<ItemVariant> storage = contraption.getSharedInventory();
+		try {
+			return ItemStorageProvider.INSTANCE.containerCache.get(storage, () -> new ItemCollector<>(JadeFabricUtils.fromItemStorage(storage, 0, target -> (Storage<ItemVariant>) target))).update(storage, level.getGameTime());
+		} catch (ExecutionException e) {
+			return null;
+		}
 	}
 
 }
