f65db88526f32e0e9a7c93f552d3e58d62bd3aa1
ZLT <98672434+ZLT9@users.noreply.github.com>
2024-12-06 18:18:58 -0300

    fix: crash with create 0.5.1-i (#25)

--- src/main/java/snownee/jade/addon/mixin/create/GoggleOverlayRendererMixin.java
+++ src/main/java/snownee/jade/addon/mixin/create/GoggleOverlayRendererMixin.java
@@ -5,8 +5,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.mojang.blaze3d.platform.Window;
-import com.mojang.blaze3d.vertex.PoseStack;
 import com.simibubi.create.content.equipment.goggles.GoggleOverlayRenderer;
 
 import snownee.jade.addon.create.CreatePlugin;
@@ -15,7 +13,7 @@ import snownee.jade.api.config.IWailaConfig;
 @Mixin(value = GoggleOverlayRenderer.class, remap = false)
 public class GoggleOverlayRendererMixin {
 	@Inject(at = @At("HEAD"), method = "renderOverlay", cancellable = true)
-	private static void jadeaddons$renderOverlay(PoseStack poseStack, float partialTicks, Window window, CallbackInfo ci) {
+	private static void jadeaddons$renderOverlay(CallbackInfo ci) {
 		if (IWailaConfig.get().getPlugin().get(CreatePlugin.GOGGLES)) {
 			ci.cancel();
 		}
